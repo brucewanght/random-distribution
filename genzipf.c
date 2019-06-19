c65b506412540d528860ea6bbda00c8ac59759f9
brucewanght <brucewanght@163.com>
2019-06-19 10:54:08 +0800

    use binary search in zipf function

--- genzipf.c
+++ genzipf.c
@@ -48,26 +48,28 @@
 #include <stdio.h>              // Needed for printf()
 #include <stdlib.h>             // Needed for exit() and ato*()
 #include <math.h>               // Needed for pow()
+#include <stdint.h>
+#include <time.h>
 
 //----- Constants -----------------------------------------------------------
 #define  FALSE          0       // Boolean false
 #define  TRUE           1       // Boolean true
 
 //----- Function prototypes -------------------------------------------------
-int      zipf(double alpha, int n);  // Returns a Zipf random variable
-double   rand_val(int seed);         // Jain's RNG
+uint64_t zipf(double alpha, uint64_t n);  // Returns a Zipf random variable
+double   rand_val(uint64_t seed);         // Jain's RNG
 
 //===== Main program ========================================================
-void main(void)
+int main(void)
 {
-  FILE   *fp;                   // File pointer to output file
+  FILE   *fp;                   // File pouint64_ter to output file
   char   file_name[256];        // Output file name string
   char   temp_string[256];      // Temporary string variable
   double alpha;                 // Alpha parameter
   double n;                     // N parameter
-  int    num_values;            // Number of values
-  int    zipf_rv;               // Zipf random variable
-  int    i;                     // Loop counter
+  uint64_t    num_values;            // Number of values
+  uint64_t    zipf_rv;               // Zipf random variable
+  uint64_t    i;                     // Loop counter
 
   // Output banner
   printf("---------------------------------------- genzipf.c ----- \n");
@@ -87,7 +89,7 @@ void main(void)
   // Prompt for random number seed and then use it
   printf("Random number seed (greater than 0) ================> ");
   scanf("%s", temp_string);
-  rand_val((int) atoi(temp_string));
+  rand_val((uint64_t) atoi(temp_string));
 
   // Prompt for alpha value
   printf("Alpha value ========================================> ");
@@ -109,16 +111,24 @@ void main(void)
   printf("-  Generating samples to file                          - \n");
   printf("-------------------------------------------------------- \n");
 
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
   // Generate and output zipf random variables
   for (i=0; i<num_values; i++)
   {
     zipf_rv = zipf(alpha, n);
-    fprintf(fp, "%d \n", zipf_rv);
+    fprintf(fp, "%lu \n", zipf_rv);
   }
+  clock_gettime(CLOCK_MONOTONIC, &end);
 
   // Output "done" message and close the output file
   printf("-------------------------------------------------------- \n");
   printf("-  Done! \n");
+  long long elapsed_time = (end.tv_sec - start.tv_sec) * 1000000000
+                           + (end.tv_nsec - start.tv_nsec);
+  elapsed_time /= 1000;
+  printf("elapsed_time: %lld, Avg: %f\n", elapsed_time,
+         (double)elapsed_time / num_values);
   printf("-------------------------------------------------------- \n");
   fclose(fp);
 }
@@ -128,14 +138,15 @@ void main(void)
 //=    - Input: alpha and N                                                 =
 //=    - Output: Returns with Zipf distributed random variable              =
 //===========================================================================
-int zipf(double alpha, int n)
+uint64_t zipf(double alpha, uint64_t n)
 {
   static int first = TRUE;      // Static first time flag
   static double c = 0;          // Normalization constant
+  static double *sum_probs;     // Pre-calculated sum of probabilities
   double z;                     // Uniform random number (0 < z < 1)
-  double sum_prob;              // Sum of probabilities
-  double zipf_value;            // Computed exponential value to be returned
+  int zipf_value;               // Computed exponential value to be returned
   int    i;                     // Loop counter
+  int low, high, mid;           // Binary-search bounds
 
   // Compute normalization constant on first call only
   if (first == TRUE)
@@ -143,6 +154,12 @@ int zipf(double alpha, int n)
     for (i=1; i<=n; i++)
       c = c + (1.0 / pow((double) i, alpha));
     c = 1.0 / c;
+
+    sum_probs = (double *)malloc((n+1)*sizeof(*sum_probs));
+    sum_probs[0] = 0;
+    for (i=1; i<=n; i++) {
+      sum_probs[i] = sum_probs[i-1] + c / pow((double) i, alpha);
+    }
     first = FALSE;
   }
 
@@ -154,23 +171,24 @@ int zipf(double alpha, int n)
   while ((z == 0) || (z == 1));
 
   // Map z to the value
-  sum_prob = 0;
-  for (i=1; i<=n; i++)
-  {
-    sum_prob = sum_prob + c / pow((double) i, alpha);
-    if (sum_prob >= z)
-    {
-      zipf_value = i;
+  low = 1, high = n, mid;
+  do {
+    mid = floor((low+high)/2);
+    if (sum_probs[mid] >= z && sum_probs[mid-1] < z) {
+      zipf_value = mid;
       break;
+    } else if (sum_probs[mid] >= z) {
+      high = mid-1;
+    } else {
+      low = mid+1;
     }
-  }
+  } while (low <= high);
 
   // Assert that zipf_value is between 1 and N
   assert((zipf_value >=1) && (zipf_value <= n));
 
   return(zipf_value);
 }
-
 //=========================================================================
 //= Multiplicative LCG for generating uniform(0.0, 1.0) random numbers    =
 //=   - x_n = 7^5*x_(n-1)mod(2^31 - 1)                                    =
@@ -178,13 +196,13 @@ int zipf(double alpha, int n)
 //=   - From R. Jain, "The Art of Computer Systems Performance Analysis," =
 //=     John Wiley & Sons, 1991. (Page 443, Figure 26.2)                  =
 //=========================================================================
-double rand_val(int seed)
+double rand_val(uint64_t seed)
 {
   const long  a =      16807;  // Multiplier
   const long  m = 2147483647;  // Modulus
   const long  q =     127773;  // m div a
   const long  r =       2836;  // m mod a
-  static long x;               // Random int value
+  static long x;               // Random uint64_t value
   long        x_div_q;         // x divided by q
   long        x_mod_q;         // x modulo q
   long        x_new;           // New x value
@@ -196,7 +214,7 @@ double rand_val(int seed)
     return(0.0);
   }
 
-  // RNG using integer arithmetic
+  // RNG using uint64_t arithmetic
   x_div_q = x / q;
   x_mod_q = x % q;
   x_new = (a * x_mod_q) - (r * x_div_q);
